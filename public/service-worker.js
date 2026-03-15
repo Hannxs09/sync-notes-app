371c07897317029e43b1dc96cd0123713976d8cd
Hannxs09 <hannes.laabs@outlook.de>
2026-03-15 22:27:49 +0100

    Design aktualisiert

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,9 @@
-self.addEventListener("install", event => {
- console.log("Service Worker installiert")
+self.addEventListener('install', event => {
+  console.log('Service Worker installiert')
+  self.skipWaiting()
+})
+
+self.addEventListener('fetch', event => {
+  // einfach weiterleiten, minimal für PWA
+  event.respondWith(fetch(event.request))
 })
